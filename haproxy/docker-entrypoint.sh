6bae8bd17835ce22e38326491f0065d2f8af50fd
AkashiSN <btorntireinvynriy@gmail.com>
2021-01-31 17:51:16 +0900

    add haproxy

---
+++ haproxy/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# iptables -t mangle -N DIVERT
+# iptables -t mangle -A PREROUTING -p tcp -m socket -j DIVERT
+# iptables -t mangle -A DIVERT -j MARK --set-mark 1
+# iptables -t mangle -A DIVERT -j ACCEPT
+
+# ip rule add fwmark 1 lookup 100
+# ip route add local 0.0.0.0/0 dev lo table 100
+
+exec "$@"
