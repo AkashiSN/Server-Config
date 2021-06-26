e8d9a5d1db48cc837ea489d99eaf3047f81ef126
AkashiSN <btorntireinvynriy@gmail.com>
2021-06-26 13:40:39 +0900

    update backup script

--- nextcloud/backup.sh
+++ nextcloud/backup.sh
@@ -2,7 +2,7 @@
 set -eu
 
 function search_file() {
-  echo $(find "$1" -maxdepth 1 -name "$2" -printf "%T+ %p\n" | sort -r | cut -d " " -f 2)
+  echo $(find "$1" -maxdepth 1 -name "$2" -printf "%T+ %p\n" | sort | cut -d " " -f 2)
 }
 
 DATETIME=$(date +"%Y%m%d%H%M%S")
@@ -15,20 +15,18 @@ DIRECTORY_BACKUP="${BACKUP_DIR}/nextcloud-dir-${DATETIME}.tar.gz"
 MAX_BACKUP=30
 
 SQL_BACKUP_LIST=($(search_file ${BACKUP_DIR} '*.sql'))
-if [ ${#SQL_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then
-  # 最新のバックアップ以外を削除する
-  for SQL_BACKUP in "${SQL_BACKUP_LIST[@]:1}"; do
+if [ ${#SQL_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then # 最大バックアップ数より多い場合古いバックアップを削除する
+  for SQL_BACKUP in "${SQL_BACKUP_LIST[@]: 0:$((${#SQL_BACKUP_LIST[@]}-${MAX_BACKUP}))}"; do
+    echo "Remove ${SQL_BACKUP}"
     rm ${SQL_BACKUP}
-    echo "Removed ${SQL_BACKUP}."
   done
 fi
 
 DIR_BACKUP_LIST=($(search_file ${BACKUP_DIR} '*.tar.gz'))
-if [ ${#DIR_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then
-  # 最新のバックアップ以外を削除する
-  for DIR_BACKUP in "${DIR_BACKUP_LIST[@]:1}"; do
+if [ ${#DIR_BACKUP_LIST[@]} -gt ${MAX_BACKUP} ]; then # 最大バックアップ数より多い場合古いバックアップを削除する
+  for DIR_BACKUP in "${DIR_BACKUP_LIST[@]: 0:$((${#DIR_BACKUP_LIST[@]}-${MAX_BACKUP}))}"; do
+    echo "Remove ${DIR_BACKUP}"
     rm ${DIR_BACKUP}
-    echo "Removed ${DIR_BACKUP}."
   done
 fi
 
