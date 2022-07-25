a716b66c79e5afa5e8b31d94a5953011c22aab4c
AkashiSN <btorntireinvynriy@gmail.com>
2022-07-25 18:21:53 +0900

    add maintenance_window_start

--- nextcloud/custom-entrypoint.sh
+++ nextcloud/custom-entrypoint.sh
@@ -13,5 +13,6 @@ run_as 'php /var/www/html/occ app:enable files_external'
 run_as 'php /var/www/html/occ app:enable twofactor_totp'
 
 run_as 'php /var/www/html/occ config:system:set default_phone_region --value=JP'
+run_as 'php /var/www/html/occ config:system:set maintenance_window_start --value=1'
 
 exec "$@"
