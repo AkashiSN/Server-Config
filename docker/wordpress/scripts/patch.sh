e2e60778acbc7d7e1292688013825c9c6de7eb64
AkashiSN <btorntireinvynriy@gmail.com>
2023-01-29 19:48:22 +0900

    update entrypoint

--- docker/wordpress/scripts/patch.sh
+++ docker/wordpress/scripts/patch.sh
@@ -1,6 +1,3 @@
-#!/bin/sh
-set -eu
-
 run_as() {
   if [ "$(id -u)" = 0 ]; then
     su -p www-data -s /bin/sh -c "$1"
@@ -10,7 +7,7 @@ run_as() {
 }
 
 if ! run_as 'wp core is-installed'; then
-  run_as 'wp core install --url=${WORDPRESS_URL} --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=info@example.com --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}'
+  run_as 'wp core install --url=${WORDPRESS_URL} --locale=ja --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=info@example.com --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}'
 fi
 
 run_as 'wp plugin install redis-cache --activate'
