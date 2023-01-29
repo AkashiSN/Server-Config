70a6bb9b46e4cd0adb79eba5ddfe96803b7fbf9a
AkashiSN <btorntireinvynriy@gmail.com>
2023-01-29 18:52:22 +0900

    update entrypoint

--- docker/wordpress/scripts/custom-entrypoint.sh
+++ docker/wordpress/scripts/custom-entrypoint.sh
@@ -9,6 +9,10 @@ run_as() {
   fi
 }
 
+if ! run_as 'wp core is-installed'; then
+  run_as 'wp core install --url=${WORDPRESS_URL} --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=info@example.com --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}'
+fi
+
 run_as 'wp plugin install redis-cache --activate'
 run_as 'wp config set WP_REDIS_HOST wordpress-redis'
 
