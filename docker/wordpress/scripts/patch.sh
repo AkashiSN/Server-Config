0b612b203f0804aeb4b193a40c39f682b0da8c82
AkashiSN <btorntireinvynriy@gmail.com>
2023-01-29 20:12:20 +0900

    update locale

--- docker/wordpress/scripts/patch.sh
+++ docker/wordpress/scripts/patch.sh
@@ -7,10 +7,13 @@ run_as() {
 }
 
 if ! run_as 'wp core is-installed'; then
-  run_as 'wp core install --url=${WORDPRESS_URL} --locale=ja --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=info@example.com --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}'
+  run_as "wp core install --url=${WORDPRESS_URL} --locale=ja --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=$(cat ${WORDPRESS_ADMIN_EMAIL}) --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}"
+  run_as "wp core language install ja --activate"
+  run_as "wp option update timezone_string $(wp eval "echo _x( '0', 'default GMT offset or timezone string' );")"
+  run_as "wp option update date_format $(wp eval "echo __( 'M jS Y' );")"
 fi
 
-run_as 'wp plugin install redis-cache --activate'
-run_as 'wp config set WP_REDIS_HOST wordpress-redis'
+run_as "wp plugin install redis-cache --activate"
+run_as "wp config set WP_REDIS_HOST wordpress-redis"
 
 exec "$@"
