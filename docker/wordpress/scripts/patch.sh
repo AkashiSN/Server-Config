dfaa2edd050c4283fe4f27d125f23196aefa8d66
AkashiSN <btorntireinvynriy@gmail.com>
2023-01-29 21:01:24 +0900

    update patch

--- docker/wordpress/scripts/patch.sh
+++ docker/wordpress/scripts/patch.sh
@@ -1,19 +1,12 @@
-run_as() {
-  if [ "$(id -u)" = 0 ]; then
-    su -p www-data -s /bin/sh -c "$1"
-  else
-    sh -c "$1"
+su -p www-data -s /bin/sh
+  if ! wp core is-installed; then
+    wp core install --url=${WORDPRESS_URL} --locale=ja --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=$(cat ${WORDPRESS_ADMIN_EMAIL_FILE}) --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}
+    wp core language install ja --activate
+    wp option update timezone_string $(wp eval "echo _x( '0', 'default GMT offset or timezone string' );")
+    wp option update date_format $(wp eval "echo __( 'M jS Y' );")
+    wp plugin install redis-cache --activate
+    wp config set WP_REDIS_HOST wordpress-redis
   fi
-}
-
-if ! run_as 'wp core is-installed'; then
-  run_as "wp core install --url=${WORDPRESS_URL} --locale=ja --title=${WORDPRESS_TITLE} --admin_user=$(cat ${WORDPRESS_ADMIN_USER_FILE}) --admin_email=$(cat ${WORDPRESS_ADMIN_EMAIL_FILE}) --prompt=admin_password < ${WORDPRESS_ADMIN_PASSWORD_FILE}"
-  run_as "wp core language install ja --activate"
-  run_as "wp option update timezone_string $(wp eval "echo _x( '0', 'default GMT offset or timezone string' );")"
-  run_as "wp option update date_format $(wp eval "echo __( 'M jS Y' );")"
-fi
-
-run_as "wp plugin install redis-cache --activate"
-run_as "wp config set WP_REDIS_HOST wordpress-redis"
+exit
 
 exec "$@"
