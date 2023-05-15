863b89b84b0f6ca22e055a99ba0b40c28dd97da0
AkashiSN <btorntireinvynriy@gmail.com>
2023-05-15 17:00:31 +0900

    fix miss

--- docker/nextcloud/scripts/init.sh
+++ docker/nextcloud/scripts/init.sh
@@ -3,7 +3,7 @@ set -eu
 
 chmod 0770 "${NEXTCLOUD_DATA_DIR}"
 
-if [ -n "${EXTERNAL_DIR}" ];
+if [ -n "${EXTERNAL_DIR}" ]; then
   chown www-data:www-data "${EXTERNAL_DIR}"
   chmod 0770 "${EXTERNAL_DIR}"
 fi
