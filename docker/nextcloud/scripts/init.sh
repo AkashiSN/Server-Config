17c1fc416dc602ccc6508cc2b3b065ae0335a902
AkashiSN <btorntireinvynriy@gmail.com>
2023-05-15 16:55:39 +0900

    update nextcloud

--- docker/nextcloud/scripts/init.sh
+++ docker/nextcloud/scripts/init.sh
@@ -3,7 +3,9 @@ set -eu
 
 chmod 0770 "${NEXTCLOUD_DATA_DIR}"
 
-chown www-data:www-data "${EXTERNAL_DIR}"
-chmod 0770 "${EXTERNAL_DIR}"
+if [ -n "${EXTERNAL_DIR}" ];
+  chown www-data:www-data "${EXTERNAL_DIR}"
+  chmod 0770 "${EXTERNAL_DIR}"
+fi
 
 exec "$@"
