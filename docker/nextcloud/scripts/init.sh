3115b5f94e6e2f449cec739694a9effc22011f0e
AkashiSN <btorntireinvynriy@gmail.com>
2023-07-26 19:13:00 +0900

    update init script

--- docker/nextcloud/scripts/init.sh
+++ docker/nextcloud/scripts/init.sh
@@ -4,9 +4,11 @@ set -eu
 chmod 0770 "${NEXTCLOUD_DATA_DIR}"
 
 set +u
-if [ -n "${EXTERNAL_DIR}" ]; then
-  chown www-data:www-data "${EXTERNAL_DIR}"
-  chmod 0770 "${EXTERNAL_DIR}"
+if [ -n "${EXTERNAL_DIRS}" ]; then
+  IFS=',' read -r -a EXTERNAL_DIRS_LIST <<< "$EXTERNAL_DIRS"
+  for dir in "${EXTERNAL_DIRS_LIST[@]}"; do
+    echo "${dir}"
+  done
 fi
 set -u
 
