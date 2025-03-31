6e65cd126a4bce806f6b8e1b6c1c4656bcac8bab
AkashiSN <btorntireinvynriy@gmail.com>
2025-03-31 20:40:24 +0900

    update k8s v1.32

--- terraform/k8s/variable.tf
+++ terraform/k8s/variable.tf
@@ -17,9 +17,10 @@ variable "userdata" {
 }
 
 locals {
-  k8s_version = 131
+  k8s_version = 132
+  base_number = local.k8s_version + 3
   k8s_control_plane = {
-    vmid                 = local.k8s_version
+    vmid                 = local.base_number
     template_vmid        = 9001
     memory               = 16384
     cores                = 6
@@ -27,13 +28,13 @@ locals {
     proxmox_node         = "pve01"
     proxmox_address      = "172.16.254.5"
     hostname             = "k8s-v${local.k8s_version}-control-plane"
-    ipv4_address         = "172.16.254.${local.k8s_version}"
+    ipv4_address         = "172.16.254.${local.base_number}"
     ipv4_prefix          = "24"
     ipv4_default_gateway = "172.16.254.1"
-    ipv6_address_token   = "::254:${local.k8s_version}"
+    ipv6_address_token   = "::254:${local.base_number}"
   }
   worker_node_01 = {
-    vmid                  = local.k8s_version + 1
+    vmid                  = local.base_number + 1
     template_vmid         = 9000
     memory                = 32768
     cores                 = 16
@@ -41,16 +42,16 @@ locals {
     proxmox_node          = "pve"
     proxmox_address       = "172.16.254.4"
     hostname              = "k8s-v${local.k8s_version}-worker-node-01"
-    ipv4_address          = "172.16.254.${local.k8s_version + 1}"
+    ipv4_address          = "172.16.254.${local.base_number + 1}"
     ipv4_prefix           = "24"
     ipv4_default_gateway  = "172.16.254.1"
-    ipv6_address_token    = "::254:${local.k8s_version + 1}"
+    ipv6_address_token    = "::254:${local.base_number + 1}"
     nas_interface_address = "172.16.255.2/24"
     nas_network_address   = "172.16.255.0/24"
     nas_network_gateway   = "172.16.255.1"
   }
   worker_node_02 = {
-    vmid                  = local.k8s_version + 2
+    vmid                  = local.base_number + 2
     template_vmid         = 9002
     memory                = 49152
     cores                 = 8
@@ -58,10 +59,10 @@ locals {
     proxmox_node          = "pve02"
     proxmox_address       = "172.16.254.6"
     hostname              = "k8s-v${local.k8s_version}-worker-node-02"
-    ipv4_address          = "172.16.254.${local.k8s_version + 2}"
+    ipv4_address          = "172.16.254.${local.base_number + 2}"
     ipv4_prefix           = "24"
     ipv4_default_gateway  = "172.16.254.1"
-    ipv6_address_token    = "::254:${local.k8s_version + 2}"
+    ipv6_address_token    = "::254:${local.base_number + 2}"
     nas_interface_address = "172.16.255.3/24"
     nas_network_address   = "172.16.255.0/24"
     nas_network_gateway   = "172.16.255.1"
