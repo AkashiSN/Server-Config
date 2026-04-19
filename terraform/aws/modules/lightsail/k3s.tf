e332edf77ffe02afdf5ec6220ee47b46b4c43b1c
AkashiSN <btorntireinvynriy@gmail.com>
2026-04-19 19:09:25 +0900

    add disk

--- terraform/aws/modules/lightsail/k3s.tf
+++ terraform/aws/modules/lightsail/k3s.tf
@@ -2,7 +2,7 @@ resource "aws_lightsail_instance" "k3s" {
   name              = "${var.project}_k3s"
   availability_zone = "ap-northeast-1a"
   blueprint_id      = "ubuntu_24_04"
-  bundle_id         = "xlarge_3_0"
+  bundle_id         = "large_3_0"
   ip_address_type   = "dualstack"
   key_pair_name     = aws_lightsail_key_pair.main.name
   user_data         = "curl -fsSL https://akashisn.info/k3s_lightsail.sh | bash"
@@ -10,6 +10,18 @@ resource "aws_lightsail_instance" "k3s" {
   tags = {
     Name = "${var.project}_k3s"
   }
+}
+
+resource "aws_lightsail_disk" "k3s_zfs" {
+  name              = "${var.project}_k3s-zfs"
+  size_in_gb        = 128
+  availability_zone = "ap-northeast-1a"
+}
+
+resource "aws_lightsail_disk_attachment" "k3s_zfs" {
+  disk_name     = aws_lightsail_disk.k3s_zfs.name
+  instance_name = aws_lightsail_instance.k3s.name
+  disk_path     = "/dev/xvdf"
 }
 
 resource "aws_lightsail_static_ip" "k3s" {
